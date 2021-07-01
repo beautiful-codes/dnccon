13183fddaa26a74d50d40bbb9ff4a5d854a24ba1
Vinay Joshi <vinay01joshi@gmail.com>
2021-07-01 22:54:59 +0530

    Adding Packages to make proejct decoupled.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dnccon
+
 Dot net core console application with Best practices.
+
+## Implementation we are doing here
+
+- Dependency Injection
+- Serilog files
+- Appsettings
+
+### Nuget Packages working
+
+- `Microsoft.Extensions.Configuration`for initializing configuration.
+- `Microsoft.Extensions.Configuration.FileExtensions`for `SetBasePath()` method
+- `Microsoft.Extensions.Configuration.Json` for `AddJsonFile()`method.
+- `Microsoft.Extensions.Configuratin.EnvironmentVrabiles` for `AddEnvironmentVariable` .
+-
--- dnccon.Client/Program.cs
+++ dnccon.Client/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace dnccon.Client
 {
@@ -7,6 +11,39 @@ namespace dnccon.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder();
+            BuildConfiguration(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+
+            Log.Logger.Information("Application Starting");
+
+            // Hosting Application in Console.
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+
+                })
+                .UseSerilog()
+                .Build();
+                
         }
+
+        static void BuildConfiguration(IConfigurationBuilder builder)
+        {
+            builder
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+              .AddEnvironmentVariables();
+        }
+
+
     }
 }
